caac95fd132828fe186bb3c63a9b04146584c387
kazurego7 <kazurego7@gmail.com>
2019-01-04 19:54:47 +0900

    practice ABC001/A-B

---
+++ Practice/BeginnerContest/ABC001/B/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace B {
+    class Program {
+        static void Main (string[] args) {
+            // input
+            var m = int.Parse (Console.ReadLine ());
+            // calc
+            var result = 0;
+            if (m < 100) {
+                result = 0;
+            } else if (100 <= m && m <= 5000) {
+                result = m / 100;
+            } else if (6000 <= m && m <= 30000) {
+                result = m / 1000 + 50;
+            } else if (35000 <= m && m <= 70000) {
+                result = (m / 1000 - 30) / 5 + 80;
+            } else {
+                result = 89;
+            }
+            Console.WriteLine ($"{result:D2}");
+        }
+    }
+}
