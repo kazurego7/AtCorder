ff0817a8e7030b9f88b004d6395c2fdabaef6019
kazurego7 <kazurego7@gmail.com>
2019-01-17 22:35:55 +0900

    practice ABC005/A-B ABC006/A

---
+++ Practice/BeginnerContest/ABC005/c/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace c {
+    class Program {
+        static void Main (string[] args) {
+            var t = int.Parse (Console.ReadLine ());
+            var n = int.Parse (Console.ReadLine ());
+            var a = Console.ReadLine ().Split (' ').Select (c => int.Parse (c)).ToList ();
+            var m = int.Parse (Console.ReadLine ());
+            var b = Console.ReadLine ().Split (' ').Select (c => int.Parse (c)).ToList ();
+
+            // calc
+            var maxAwithinB = b.Select (bi => a.TakeWhile (ai => ai <= bi).Last ()).ToList ();
+            var isYes = false;
+            if (n < m) {
+                isYes = false;
+            } else {
+                isYes = Enumerable.Range (0, b.Count).All (i => b[i] - maxAwithinB[i] >= t);
+            }
+
+        }
+    }
+}
