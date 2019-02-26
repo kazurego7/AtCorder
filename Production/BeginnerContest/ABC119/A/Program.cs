fd09bd5cdf57ce70d16e686752e3fde8d2b1b061
kazurego7 <kazurego7@gmail.com>
2019-02-26 17:05:36 +0900

    practice ABC007 A/ABC008 A-B/production ABC119 A-B-C
    production ABC119 C は未完

---
+++ Production/BeginnerContest/ABC119/A/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace A {
+    class Program {
+        static void Main (string[] args) {
+            var s = Console.ReadLine ();
+            var dateInfos = s.Split ('/').Select (str => int.Parse (str)).ToList ();
+            var yyyy = dateInfos[0];
+            var mm = dateInfos[1];
+            var dd = dateInfos[2];
+
+            if (yyyy != 2019) {
+                if (yyyy < 2019) {
+                    Console.WriteLine ("Heisei");
+                } else {
+                    Console.WriteLine ("TBD");
+                }
+            } else if (mm != 4) {
+                if (mm < 4) {
+                    Console.WriteLine ("Heisei");
+                } else {
+                    Console.WriteLine ("TBD");
+                }
+            } else {
+                if (dd <= 30) {
+                    Console.WriteLine ("Heisei");
+                } else {
+                    Console.WriteLine ("TBD");
+                }
+            }
+        }
+    }
+}
---
+++ Production/BeginnerContest/ABC119/B/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace B {
+    class Program {
+        static void Main (string[] args) {
+            var n = int.Parse (Console.ReadLine ());
+            var xs = new List<double> ();
+            var us = new List<string> ();
+            foreach (var i in Enumerable.Range (0, n)) {
+                var xu = Console.ReadLine ().Split (' ');
+                var x = xu[0];
+                var u = xu[1];
+                xs.Add (double.Parse (x));
+                us.Add (u);
+            }
+
+            var fullAmount = 0.0;
+            foreach (var i in Enumerable.Range (0, n)) {
+                if (us[i] == "JPY") {
+                    fullAmount += xs[i];
+                } else {
+                    var bc2yen = xs[i] * 380000.0;
+                    fullAmount += bc2yen;
+                }
+            }
+
+            Console.WriteLine (fullAmount);
+        }
+    }
+}
