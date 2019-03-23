e6eb2c3c276590a6d0334d07063428859d2c8d85
kazurego7 <kazurego7@gmail.com>
2019-03-23 20:52:37 +0900

    practice ABC018 A

---
+++ Practice/BeginnerContest/ABC018/D/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AtCoderTemplate {
+    class Program {
+        static void Main (string[] args) { }
+        static int ReadInt () {
+            return int.Parse (Console.ReadLine ());
+        }
+        static List<int> ReadInts () {
+            return Console.ReadLine ().Split (' ').Select (c => int.Parse (c)).ToList ();
+        }
+        static List<List<int>> ReadSequence (int n) {
+            /*
+            入力例
+            A1 B1
+            A2 B2
+            ...
+            An Bn
+
+            出力例
+            [[A1,A2,...,An], [B1,B2,...,Bn]]
+            */
+            var seq = Enumerable.Range (0, n).Select (i => ReadInts ()).ToList ();
+            return Enumerable.Range (0, seq.First ().Count ()).Select (i => seq.Select (items => items[i]).ToList ()).ToList ();
+        }
+        static void PrintList<T> (IEnumerable<T> list) {
+            foreach (var item in list) {
+                Console.WriteLine (item);
+            }
+        }
+    }
+}
